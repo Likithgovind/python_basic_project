2ba4be3afe130f2afdfd2d29a8864cc5783c47f6
Likithgovind <likigovind@gmail.com>
2025-01-30 18:30:14 +0530

    convertibg number to word less than 1000 was created

---
+++ number to word.py
@@ -0,0 +1,29 @@
+#convertibg number to word less than 1000
+numbers=int(input("enter the number: "))
+
+ones=["","one","two","three","four","five","six","seven","eight","nine"]
+teens = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+
+if numbers==0:
+    print("zero")
+
+else:
+    words=[]
+
+    if numbers//100:
+        words.append(ones[numbers//100]+" hundered")
+        numbers%=100
+    if numbers>=10 and numbers<20:
+        words.append(teens[numbers%10])
+        numbers=0
+    elif numbers>=20:
+        words.append(tens[numbers//10])
+        numbers%=10
+    if numbers>0:
+        words.append(ones[numbers])
+
+    print(" ".join(words))
+
+
+
